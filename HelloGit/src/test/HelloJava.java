ebd4d56174209e90ba47f4fe953d849309393d3d
user <user@user-PC>
2017-02-02 11:07:14 +0900

    Second Commit Work

--- HelloGit/src/test/HelloJava.java
+++ HelloGit/src/test/HelloJava.java
@@ -3,10 +3,12 @@ package test;
 public class HelloJava {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// 1차 commit 내용
 		System.out.println("Hello Java :) ");
 		System.out.println("First Commit Work");
 		
+		// brench 추가 내용
+		System.out.println("Second Commit Work");
 	}
 
 }
